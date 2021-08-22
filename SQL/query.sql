4a436be8459994a5cdf2cc881063906308fb3613
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2021-08-21 18:49:12 -0700

    Delete SQL directory

--- SQL/query.sql
+++
@@ -1,5 +0,0 @@
-SELECT employee.id,first_name,last_name,department.name,title,salary,manager_id 
-FROM department
-JOIN role ON department.id = role.department_id
-JOIN employee ON role.id = employee.role_id;
-
